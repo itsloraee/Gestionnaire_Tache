9c07264873e292d3108b2d623e043b02b107c0f4
itsloraee <laurxee@gmail.com>
2025-10-28 16:15:43 -0400

    Re création de la migration

--- database/migrations/2025_10_28_200800_create_tasks_table.php
+++ database/migrations/2025_10_28_200800_create_tasks_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->boolean('statut')->default(false);
             $table->timestamps();
         });
     }
